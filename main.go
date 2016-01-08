d7f0053fb83f2bd48c6b06d3980fbabadb81718d
Anselm Strauss <strauss@puzzle.ch>
2016-01-08 16:37:33 +0100

    benchmarked modifying vs. returning new instance versions of multiply methods

--- main.go
+++ main.go
@@ -1,10 +1,42 @@
 package main
 
+import (
+	"math/rand"
+)
+
 type Vector [3]float64
+
+func RandVec(r *rand.Rand) *Vector {
+	v := Vector{}
+	for i := 0; i < 3; i++ {
+		v[i] = r.Float64()
+	}
+	return &v
+}
+
+// MulVec multiplies the given matrix with the vector and modifies it.
+func (v *Vector) MulVec(m *Matrix) {
+	x := m[0]*v[0] + m[1]*v[1] + m[2]*v[2]
+	y := m[3]*v[0] + m[4]*v[1] + m[5]*v[2]
+	z := m[6]*v[0] + m[7]*v[1] + m[8]*v[2]
+	v[0] = x
+	v[1] = y
+	v[2] = z
+}
 
 type Matrix [9]float64
 
-func (m *Matrix) Mulv(v *Vector) *Vector {
+func RandMat(r *rand.Rand) *Matrix {
+	m := Matrix{}
+	for i := 0; i < 9; i++ {
+		m[i] = r.Float64()
+	}
+	return &m
+}
+
+// MulVecNew multiplies the matrix with a vector and returns a new vector.
+// MulVecNew Is slower than MulVec.
+func (m *Matrix) MulVecNew(v *Vector) *Vector {
 	return &Vector{
 		m[0]*v[0] + m[1]*v[1] + m[2]*v[2],
 		m[3]*v[0] + m[4]*v[1] + m[5]*v[2],
@@ -12,7 +44,31 @@ func (m *Matrix) Mulv(v *Vector) *Vector {
 	}
 }
 
-func (m *Matrix) Mul(n *Matrix) *Matrix {
+// MulMat multiples the matrix with another matrix and modifies the former one.
+func (m *Matrix) MulMat(n *Matrix) {
+	m0 := m[0]*n[0] + m[1]*n[3] + m[2]*n[6]
+	m1 := m[0]*n[1] + m[1]*n[4] + m[2]*n[7]
+	m2 := m[0]*n[2] + m[1]*n[5] + m[2]*n[8]
+	m3 := m[3]*n[0] + m[4]*n[3] + m[5]*n[6]
+	m4 := m[3]*n[1] + m[4]*n[4] + m[5]*n[7]
+	m5 := m[3]*n[2] + m[4]*n[5] + m[5]*n[8]
+	m6 := m[6]*n[0] + m[7]*n[3] + m[8]*n[6]
+	m7 := m[6]*n[1] + m[7]*n[4] + m[8]*n[7]
+	m8 := m[6]*n[2] + m[7]*n[5] + m[8]*n[8]
+	m[0] = m0
+	m[1] = m1
+	m[2] = m2
+	m[3] = m3
+	m[4] = m4
+	m[5] = m5
+	m[6] = m6
+	m[7] = m7
+	m[8] = m8
+}
+
+// MulMatNew multiples the matrix with another matrix and returns a new matrix.
+// MulMatNew is slower than MulMat.
+func (m *Matrix) MulMatNew(n *Matrix) *Matrix {
 	return &Matrix{
 		m[0]*n[0] + m[1]*n[3] + m[2]*n[6],
 		m[0]*n[1] + m[1]*n[4] + m[2]*n[7],
--- main_test.go
+++ main_test.go
@@ -1,22 +1,23 @@
 package main
 
 import (
+	"math/rand"
 	"testing"
 )
 
-func TestMulv(t *testing.T) {
+func TestMulVec(t *testing.T) {
 	m := Matrix{0, 1, 2, 3, 4, 5, 6, 7, 8}
 	v := Vector{0, 1, 2}
-	w := m.Mulv(&v)
-	if w[0] != 5 || w[1] != 14 || w[2] != 23 {
+	v.MulVec(&m)
+	if v[0] != 5 || v[1] != 14 || v[2] != 23 {
 		t.Fail()
 	}
 }
 
-func TestMul(t *testing.T) {
+func TestMulMatNew(t *testing.T) {
 	m := Matrix{0, 1, 2, 3, 4, 5, 6, 7, 8}
 	n := Matrix{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	o := m.Mul(&n)
+	o := m.MulMatNew(&n)
 	if o[0] != 18 ||
 		o[1] != 21 ||
 		o[2] != 24 ||
@@ -27,5 +28,45 @@ func TestMul(t *testing.T) {
 		o[7] != 111 ||
 		o[8] != 132 {
 		t.Fail()
+	}
+}
+
+func BenchmarkMulVec(b *testing.B) {
+	r := rand.New(rand.NewSource(0))
+	v := RandVec(r)
+	m := RandMat(r)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		v.MulVec(m)
+	}
+}
+
+func BenchmarkMulVecNew(b *testing.B) {
+	r := rand.New(rand.NewSource(0))
+	v := RandVec(r)
+	m := RandMat(r)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m.MulVecNew(v)
+	}
+}
+
+func BenchmarkMulMat(b *testing.B) {
+	r := rand.New(rand.NewSource(0))
+	m := RandMat(r)
+	n := RandMat(r)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m.MulMat(n)
+	}
+}
+
+func BenchmarkMulMatNew(b *testing.B) {
+	r := rand.New(rand.NewSource(0))
+	m := RandMat(r)
+	n := RandMat(r)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m.MulMatNew(n)
 	}
 }
