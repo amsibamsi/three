dc1339de44e09ce9c6784634eebb9a7ad4d74776
Anselm Strauss <strauss@puzzle.ch>
2016-02-28 19:36:52 +0100

    added matrix dot product and vector projection

--- graphics.go
+++ graphics.go
@@ -1,12 +1,75 @@
 // Render 3D graphics
 package graphics
 
-import ()
+import (
+	"math/rand"
+)
 
-// Vector with homogeneous coordinates.
+// Vector with homogeneous coordinates
 // Holds 4 components: x, y, z and w in this order.
 type Vec4 [4]float64
 
-// Matrix with homogeneous coordinates.
+// ZeroVec returns a new vector with x/z/y set to 0 and w set to 1.
+func ZeroVec() *Vec4 {
+	return &Vec4{0, 0, 0, 1}
+}
+
+// Matrix with homogeneous coordinates
 // Holds 16 components, the 4 first elements make up the first row from left to right, and so on.
 type Mat4 [16]float64
+
+// ZeroMat returns a new matrix with all values set to zero.
+func ZeroMat() *Mat4 {
+	return &Mat4{
+		0, 0, 0, 0,
+		0, 0, 0, 0,
+		0, 0, 0, 0,
+		0, 0, 0, 0,
+	}
+}
+
+// RandMat returns a new matrix random values.
+func RandMat(r *rand.Rand) *Mat4 {
+	m := Mat4{}
+	for i := 0; i < len(m); i++ {
+		m[i] = r.Float64()
+	}
+	return &m
+}
+
+// TranslMat returns a translation matrix that translates vectors by the given values for x/y/z axes.
+func TranslMat(x, y, z float64) *Mat4 {
+	return &Mat4{
+		1, 0, 0, x,
+		0, 1, 0, y,
+		0, 0, 1, z,
+		0, 0, 0, 1,
+	}
+}
+
+// Mul multiplies the matrix with another one, modifying the former one.
+func (m *Mat4) Mul(n *Mat4) {
+	t := ZeroMat()
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			for k := 0; k < 4; k++ {
+				t[i*4+j] += m[i*4+k] * n[j+k*4]
+			}
+		}
+	}
+	for i := 0; i < 16; i++ {
+		m[i] = t[i]
+	}
+}
+
+// Proj projects a given vector with the projection matrix.
+// Returns a new vector.
+func (m *Mat4) Proj(v *Vec4) *Vec4 {
+	p := Vec4{0, 0, 0, 0}
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			p[i] += m[i*4+j] * v[j]
+		}
+	}
+	return &p
+}
--- graphics_test.go
+++ graphics_test.go
@@ -1,3 +1,36 @@
 package graphics
 
-import ()
+import (
+	"math/rand"
+	"testing"
+)
+
+func TestMul(t *testing.T) {
+	m := Mat4{0, 3, 0, 1, 6, 3, 5, 3, 7, 4, 8, 7, 3, 6, 0, 3}
+	n := Mat4{9, 0, 4, 10, 4, 7, 0, 5, 6, 5, 8, 7, 9, 10, 7, 10}
+	r := Mat4{21, 31, 7, 25, 123, 76, 85, 140, 190, 138, 141, 216, 78, 72, 33, 90}
+	m.Mul(&n)
+	if m != r {
+		t.Errorf("expected '%v' but got '%v'", r, m)
+	}
+}
+
+func TestProj(t *testing.T) {
+	m := Mat4{1, 3, 2, 2, 9, 10, 1, 9, 0, 4, 5, 1, 6, 8, 5, 8}
+	v := Vec4{10, 7, 0, 8}
+	r := Vec4{47, 232, 36, 180}
+	p := *m.Proj(&v)
+	if p != r {
+		t.Errorf("expected '%v' but got '%v'", r, p)
+	}
+}
+
+func BenchmarkMul(b *testing.B) {
+	r := rand.New(rand.NewSource(0))
+	m := RandMat(r)
+	n := RandMat(r)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m.Mul(n)
+	}
+}
