ec16a8eb5602f8deebd3108414606f1200fae13f
Anselm Strauss <amsibamsi@gmail.com>
2016-03-20 20:10:38 +0100

    removed rotation stuff, added untested view/camera transformation with lots of comments, added vec3 and switched from vec4 where appropriate

--- graphics.go
+++ graphics.go
@@ -6,17 +6,56 @@ import (
 	"math/rand"
 )
 
-// Vector with homogeneous coordinates
+// Vec3 is a vector in 3D space with cartesian coordinates.
+// Holds 3 components: x, y and z in this order.
+type Vec3 [3]float64
+
+// Norm normalizes a vector to length 1 keeping its direction.
+func (v *Vec3) Norm() {
+	abs := math.Sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2])
+	v[0] /= abs
+	v[1] /= abs
+	v[2] /= abs
+}
+
+// Neg negates the vector's components.
+func (v *Vec3) Neg() {
+	v[0] = -v[0]
+	v[1] = -v[1]
+	v[2] = -v[2]
+}
+
+// Sub subtracts another vector.
+func (v *Vec3) Sub(w *Vec3) {
+	v[0] -= w[0]
+	v[1] -= w[1]
+	v[2] -= w[2]
+}
+
+// Cross returns the cross product of
+// this vector with the argument as new vector.
+func (v *Vec3) Cross(w *Vec3) *Vec3 {
+	return &Vec3{
+		v[1]*w[2] - v[2]*w[1],
+		v[2]*w[0] - v[0]*w[2],
+		v[0]*w[1] - v[1]*w[0],
+	}
+}
+
+// Vec4 is a vector in 3D space with homogeneous coordinates.
 // Holds 4 components: x, y, z and w in this order.
 type Vec4 [4]float64
 
-// NewVec returns a new vector with given x/y/z and w set to 1.
-func NewVec(x, y, z float64) *Vec4 {
+// NewVec4 returns a new vector with homogeneous coordinates
+// corresponding to the given cartesian coordinates (w will be 1).
+func NewVec4(x, y, z float64) *Vec4 {
 	return &Vec4{x, y, z, 1}
 }
 
-// Matrix with homogeneous coordinates
-// Holds 16 components, the 4 first elements make up the first row from left to right, and so on.
+// Mat4 is a matrix with homogeneous coordinates
+// used to transform homogeneous vectors.
+// Holds 16 components, the 4 first elements make up
+// the first row from left to right, and so on.
 type Mat4 [16]float64
 
 // ZeroMat returns a new matrix with all values set to zero.
@@ -38,36 +77,13 @@ func RandMat(r *rand.Rand) *Mat4 {
 	return &m
 }
 
-// TranslMat returns a new translation matrix that translates vectors by the given values for x/y/z axes.
-func TranslMat(x, y, z float64) *Mat4 {
+// TranslTransf returns a new translation matrix that
+// translates vectors by the argument vector.
+func TranslTransf(v *Vec3) *Mat4 {
 	return &Mat4{
-		1, 0, 0, x,
-		0, 1, 0, y,
-		0, 0, 1, z,
-		0, 0, 0, 1,
-	}
-}
-
-// RxMat returns a new rotation matrix that rotates about the x axis by a radians.
-func RxMat(a float64) *Mat4 {
-	s := math.Sin(a)
-	c := math.Cos(a)
-	return &Mat4{
-		1, 0, 0, 0,
-		0, c, -s, 0,
-		0, s, c, 0,
-		0, 0, 0, 1,
-	}
-}
-
-// RyMat returns a new rotation matrix that rotates about the y axis by a radians.
-func RyMat(a float64) *Mat4 {
-	s := math.Sin(a)
-	c := math.Cos(a)
-	return &Mat4{
-		c, 0, s, 0,
-		0, 1, 0, 0,
-		-s, 0, c, 0,
+		1, 0, 0, v[0],
+		0, 1, 0, v[1],
+		0, 0, 1, v[2],
 		0, 0, 0, 1,
 	}
 }
@@ -94,4 +110,106 @@ func (m *Mat4) Transf(v *Vec4) *Vec4 {
 		}
 	}
 	return &p
+}
+
+// CoordTransf returns a new matrix that transforms vectors to a new basis with
+// axes specified as x, y, z arguments which must be orthonormal.
+//
+// Reference is the standard basis with the axes (1,0,0), (0,1,0) and (0,0,1).
+// A new basis is formed from the argument axes given in standard coordinates.
+// Any vector in the standard basis is interpreted as seen by the new basis
+// and its coordinates are transformed to this new basis. This corresponds to
+// the inverse transformation of the standard basis into the new basis. We
+// see a vector through the view of the new basis, transform both back to the
+// standard basis and get the standard coordinates of the vector as seen by
+// the new basis.
+//
+// Transforming from the standard basis into the new basis is just a matter of
+// writing the standard coordinates of a vector as linear combination of the
+// new axes. E.g. a vector (v0,v1,v2) can be written as v0 * (1,0,0) + v1 *
+// (0,1,0) + v2 * (0,0,1) in the standard basis. And in the new basis with
+// axes (a0,a1,a2), (b0,b1,b2) and (c0,c1,c2) as v0 * (a0,a1,a2) + v1 *
+// (b0,b1,b2) + v2 * (c0,c1,c2). So the x coordinate of the new vector is
+// v0*a0 + v1*b0 + v2*c0 and similar for y and z coordinates. Thus the 3 new
+// basis vectors make up the columns of the transformation matrix. The final
+// matrix is then the inverted transformation, so the transposed matrix where
+// the axes of the new bases are the rows, since we want to transform vectors
+// as seen by the new basis back to the standard basis to get the standard
+// coordinates. Finally we fill up the matrix with 0 an 1 to make it
+// homogeneous.
+func CoordTransf(x, y, z *Vec3) *Mat4 {
+	return &Mat4{
+		x[0], x[1], x[2], 0,
+		y[0], y[1], y[2], 0,
+		z[0], z[1], z[2], 0,
+		0, 0, 0, 1,
+	}
+}
+
+// Camera describes a view in space.
+// It is used to create a 2D image from the scene.
+//
+// The coordinate system of a camera is as follows:
+//   - Center at Eye
+//   - Positive y along Up
+//   - Negative z along At
+//   - Positive x along the cross product of y and z (to the right)
+type Camera struct {
+
+	// Eye is the position of the eye to look from.
+	// For each object to project a virtual line to the eye is drawn.
+	Eye Vec3
+
+	// At is the direction to look at from the eye.
+	At Vec3
+
+	// Up determines the orientation of the view.
+	// Up not being perpendicular to At results in the same orientation as if Up
+	// was first projected to the normal plane of At through Eye.
+	Up Vec3
+
+	// Near is the distance from the eye in the looking direction
+	// where the orthogonal plane is set to project onto.
+	Near float64
+
+	// Far is the maximum distance from the eye in the looking direction
+	// of objects that will be drawn. Everything further away will
+	// not be drawn.
+	Far float64
+
+	// Fovx is the horizontal field of view in radian degrees.
+	Fovx float64
+
+	// Fovy is the vertical field of view in radian degrees.
+	Fovy float64
+}
+
+// CamAxes returns the 3 axes that make up the orthonormal basis
+// of the cameras right-handed coordinate system.
+func (c *Camera) CamAxes() (*Vec3, *Vec3, *Vec3) {
+	z := c.Eye
+	z.Sub(&c.At)
+	z.Norm()
+	x := c.Up
+	x.Cross(&z)
+	x.Norm()
+	// Recompute up, c.Up might not be perpendicular or normalized.
+	y := z
+	y.Cross(&x)
+	return &x, &y, &z
+}
+
+// CamTransf returns a new matrix that transforms from world coordinates into
+// view coordinates of the camera. It is the inverse transformation of getting
+// from the world view (defined as x to the right, y up and looking down -z)
+// to the camera view. That means first translating the eye of the camera to
+// the origin and then rotating the camera to match the world view.
+func (c *Camera) CamTransf() *Mat4 {
+	x, y, z := c.CamAxes()
+	m := CoordTransf(x, y, z)
+	e := c.Eye
+	e.Neg()
+	t := TranslTransf(&e)
+	m.Mul(t)
+	return m
 }
--- graphics_test.go
+++ graphics_test.go
@@ -3,21 +3,50 @@
 package graphics
 
 import (
-	"math"
 	"math/rand"
 	"testing"
 )
 
-func TestNewVec(t *testing.T) {
-	v := *NewVec(1, 2, 3)
-	r := Vec4{1, 2, 3, 1}
+func TestNorm(t *testing.T) {
+	v := Vec3{10, 0, 0}
+	r := Vec3{1, 0, 0}
+	v.Norm()
 	if v != r {
 		t.Errorf("expected '%v' but got '%v'", r, v)
 	}
 }
 
-func TestTranslMat(t *testing.T) {
-	m := *TranslMat(1, 2, 3)
+func TestNeg(t *testing.T) {
+	v := Vec3{1, -2, 0}
+	r := Vec3{-1, 2, 0}
+	v.Neg()
+	if v != r {
+		t.Errorf("expected '%v' but got '%v'", r, v)
+	}
+}
+
+func TestSub(t *testing.T) {
+	v := Vec3{50, -2, 7}
+	w := Vec3{1, 1, -6}
+	r := Vec3{49, -3, 13}
+	v.Sub(&w)
+	if v != r {
+		t.Errorf("expected '%v' but got '%v'", r, v)
+	}
+}
+
+func TestCross(t *testing.T) {
+	v1 := Vec3{2, 3, 4}
+	v2 := Vec3{5, 6, 7}
+	v3 := *v1.Cross(&v2)
+	r := Vec3{-3, 6, -3}
+	if v3 != r {
+		t.Errorf("expected '%v' but got '%v'", r, v3)
+	}
+}
+
+func TestTranslTransf(t *testing.T) {
+	m := *TranslTransf(&Vec3{1, 2, 3})
 	r := Mat4{
 		1, 0, 0, 1,
 		0, 1, 0, 2,
@@ -28,33 +57,6 @@ func TestTranslMat(t *testing.T) {
 		t.Errorf("expected '%v' but got '%v'", r, m)
 	}
 }
-
-func TestRxMat(t *testing.T) {
-	m := *RxMat(0)
-	r := Mat4{
-		1, 0, 0, 0,
-		0, 1, 0, 0,
-		0, 0, 1, 0,
-		0, 0, 0, 1,
-	}
-	if m != r {
-		t.Errorf("expected '%v' but got '%v'", r, m)
-	}
-}
-
-func TestRyMat(t *testing.T) {
-	m := *RyMat(0)
-	r := Mat4{
-		1, 0, 0, 0,
-		0, 1, 0, 0,
-		0, 0, 1, 0,
-		0, 0, 0, 1,
-	}
-	if m != r {
-		t.Errorf("expected '%v' but got '%v'", r, m)
-	}
-}
-
 func TestMul(t *testing.T) {
 	m := Mat4{0, 3, 0, 1, 6, 3, 5, 3, 7, 4, 8, 7, 3, 6, 0, 3}
 	n := Mat4{9, 0, 4, 10, 4, 7, 0, 5, 6, 5, 8, 7, 9, 10, 7, 10}
@@ -77,33 +79,11 @@ func TestTransf(t *testing.T) {
 
 func TestTransl(t *testing.T) {
 	v := Vec4{2, 1, 3, 1}
-	m := TranslMat(7, 1, -3)
+	m := TranslTransf(&Vec3{7, 1, -3})
 	r := Vec4{9, 2, 0, 1}
 	w := *m.Transf(&v)
 	if w != r {
 		t.Errorf("expected '%v' but got '%v'", r, w)
-	}
-}
-
-func TestRx(t *testing.T) {
-	v := Vec4{1, 1, 0, 1}
-	m := RxMat(math.Pi)
-	r := [4]int{1, -1, 0, 1}
-	w := *m.Transf(&v)
-	wi := [4]int{int(w[0]), int(w[1]), int(w[2]), int(w[3])}
-	if wi != r {
-		t.Errorf("expected '%v' but got '%v'", r, wi)
-	}
-}
-
-func TestRy(t *testing.T) {
-	v := Vec4{1, 1, 0, 1}
-	m := RyMat(math.Pi / 2)
-	r := [4]int{0, 1, -1, 1}
-	w := *m.Transf(&v)
-	wi := [4]int{int(w[0]), int(w[1]), int(w[2]), int(w[3])}
-	if wi != r {
-		t.Errorf("expected '%v' but got '%v'", r, wi)
 	}
 }
 
